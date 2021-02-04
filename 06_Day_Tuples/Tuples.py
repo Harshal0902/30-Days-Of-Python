5dd60b5d308b0bae56e87c00ca7d5f67822409fd
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2021-02-04 23:24:34 +0530

    Add files via upload

---
+++ 06_Day_Tuples/Tuples.py
@@ -0,0 +1,37 @@
+# Changing Tuples to Lists
+tpl = ('item1', 'item2', 'item3','item4')
+lst = list(tpl)
+fruits = ('banana', 'orange', 'mango', 'lemon')
+fruits = list(fruits)
+fruits[0] = 'apple'
+print(fruits)     # ['apple', 'orange', 'mango', 'lemon']
+fruits = tuple(fruits)
+print(fruits)     # ('apple', 'orange', 'mango', 'lemon')
+
+
+# Checking an Item in a List
+tpl = ('item1', 'item2', 'item3','item4')
+'item2' in tpl # True
+fruits = ('banana', 'orange', 'mango', 'lemon')
+print('orange' in fruits) # True
+print('apple' in fruits) # False
+fruits[0] = 'apple' # TypeError: 'tuple' object does not support item assignment
+
+
+# Joining Tuples
+tpl1 = ('item1', 'item2', 'item3')
+tpl2 = ('item4', 'item5','item6')
+tpl3 = tpl1 + tpl2
+fruits = ('banana', 'orange', 'mango', 'lemon')
+vegetables = ('Tomato', 'Potato', 'Cabbage','Onion', 'Carrot')
+fruits_and_vegetables = fruits + vegetables
+
+
+# Deleting Tuples
+tpl1 = ('item1', 'item2', 'item3')
+del tpl1
+fruits = ('banana', 'orange', 'mango', 'lemon')
+del fruits
+
+
+# 
