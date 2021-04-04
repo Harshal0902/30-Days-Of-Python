c044f23b22b6eaac2a656d5b9270fb8a571358ec
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2021-04-04 19:56:44 +0530

    Add files via upload

---
+++ 29_Day_Building_API/29_building_API.py
@@ -0,0 +1,372 @@
+# Retrieving data using get
+from flask import Flask,  Response
+import json
+
+app = Flask(__name__)
+
+@app.route('/api/v1.0/students', methods = ['GET'])
+def students ():
+    student_list = [
+        {
+            'name':'Harshal',
+            'country':'India',
+            'city':'Bangalore',
+            'skills':['HTML', 'CSS','JavaScript','Python']
+        },
+        {
+            'name':'David',
+            'country':'UK',
+            'city':'London',
+            'skills':['Python','MongoDB']
+        },
+        {
+            'name':'John',
+            'country':'Sweden',
+            'city':'Stockholm',
+            'skills':['Java','C#']
+        }
+    ]
+    return Response(json.dumps(student_list), mimetype='application/json')
+
+
+if __name__ == '__main__':
+    # for deployment
+    # to make it work for both production and development
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+
+# In stead of displaying dummy data let's connect the flask application with MongoDB and let's get data from mongoDB database.
+from flask import Flask,  Response
+import json
+import pymongo
+
+
+app = Flask(__name__)
+
+#
+MONGODB_URI='mongodb+srv://harshal:your_password@30daysofpython-twxkr.mongodb.net/test?retryWrites=true&w=majority'
+client = pymongo.MongoClient(MONGODB_URI)
+db = client['thirty_days_of_python'] # accessing the database
+
+@app.route('/api/v1.0/students', methods = ['GET'])
+def students ():
+
+    return Response(json.dumps(student), mimetype='application/json')
+
+
+if __name__ == '__main__':
+    # for deployment
+    # to make it work for both production and development
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+
+# By connecting the flask, we can fetch students collection data from the thirty_days_of_python database.
+
+[
+    {
+        "_id": {
+            "$oid": "5df68a21f106fe2d315bbc8b"
+        },
+        "name": "Harshal",
+        "country": "India",
+        "city": "Bangalore",
+        "age": 38
+    },
+    {
+        "_id": {
+            "$oid": "5df68a23f106fe2d315bbc8c"
+        },
+        "name": "David",
+        "country": "UK",
+        "city": "London",
+        "age": 34
+    },
+    {
+        "_id": {
+            "$oid": "5df68a23f106fe2d315bbc8e"
+        },
+        "name": "Sami",
+        "country": "India",
+        "city": "Bangalore",
+        "age": 25
+    }
+]
+
+
+# Getting a document by id
+from flask import Flask,  Response
+import json
+from bson.objectid import ObjectId
+import json
+from bson.json_util import dumps
+import pymongo
+
+
+app = Flask(__name__)
+
+#
+MONGODB_URI='mongodb+srv://harshal:your_password@30daysofpython-twxkr.mongodb.net/test?retryWrites=true&w=majority'
+client = pymongo.MongoClient(MONGODB_URI)
+db = client['thirty_days_of_python'] # accessing the database
+
+@app.route('/api/v1.0/students', methods = ['GET'])
+def students ():
+
+    return Response(json.dumps(student), mimetype='application/json')
+@app.route('/api/v1.0/students/<id>', methods = ['GET'])
+def single_student (id):
+    student = db.students.find({'_id':ObjectId(id)})
+    return Response(dumps(student), mimetype='application/json')
+
+if __name__ == '__main__':
+    # for deployment
+    # to make it work for both production and development
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+[
+    {
+        "_id": {
+            "$oid": "5df68a21f106fe2d315bbc8b"
+        },
+        "name": "Harshal",
+        "country": "India",
+        "city": "Bangalore",
+        "age": 38
+    }
+]
+
+
+# Creating data using POST
+from flask import Flask,  Response
+import json
+from bson.objectid import ObjectId
+import json
+from bson.json_util import dumps
+import pymongo
+from datetime import datetime
+
+
+app = Flask(__name__)
+
+#
+MONGODB_URI='mongodb+srv://harshal:your_password@30daysofpython-twxkr.mongodb.net/test?retryWrites=true&w=majority'
+client = pymongo.MongoClient(MONGODB_URI)
+db = client['thirty_days_of_python'] # accessing the database
+
+@app.route('/api/v1.0/students', methods = ['GET'])
+def students ():
+
+    return Response(json.dumps(student), mimetype='application/json')
+@app.route('/api/v1.0/students/<id>', methods = ['GET'])
+def single_student (id):
+    student = db.students.find({'_id':ObjectId(id)})
+    return Response(dumps(student), mimetype='application/json')
+@app.route('/api/v1.0/students', methods = ['POST'])
+def create_student ():
+    name = request.form['name']
+    country = request.form['country']
+    city = request.form['city']
+    skills = request.form['skills'].split(', ')
+    bio = request.form['bio']
+    birthyear = request.form['birthyear']
+    created_at = datetime.now()
+    student = {
+        'name': name,
+        'country': country,
+        'city': city,
+        'birthyear': birthyear,
+        'skills': skills,
+        'bio': bio,
+        'created_at': created_at
+
+    }
+    db.students.insert_one(student)
+    return ;
+def update_student (id):
+if __name__ == '__main__':
+    # for deployment
+    # to make it work for both production and development
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+
+# Updating using PUT
+from flask import Flask,  Response
+import json
+from bson.objectid import ObjectId
+import json
+from bson.json_util import dumps
+import pymongo
+from datetime import datetime
+
+
+app = Flask(__name__)
+
+#
+MONGODB_URI='mongodb+srv://harshal:your_password@30daysofpython-twxkr.mongodb.net/test?retryWrites=true&w=majority'
+client = pymongo.MongoClient(MONGODB_URI)
+db = client['thirty_days_of_python'] # accessing the database
+
+@app.route('/api/v1.0/students', methods = ['GET'])
+def students ():
+
+    return Response(json.dumps(student), mimetype='application/json')
+@app.route('/api/v1.0/students/<id>', methods = ['GET'])
+def single_student (id):
+    student = db.students.find({'_id':ObjectId(id)})
+    return Response(dumps(student), mimetype='application/json')
+@app.route('/api/v1.0/students', methods = ['POST'])
+def create_student ():
+    name = request.form['name']
+    country = request.form['country']
+    city = request.form['city']
+    skills = request.form['skills'].split(', ')
+    bio = request.form['bio']
+    birthyear = request.form['birthyear']
+    created_at = datetime.now()
+    student = {
+        'name': name,
+        'country': country,
+        'city': city,
+        'birthyear': birthyear,
+        'skills': skills,
+        'bio': bio,
+        'created_at': created_at
+
+    }
+    db.students.insert_one(student)
+    return
+@app.route('/api/v1.0/students/<id>', methods = ['PUT']) # this decorator create the home route
+def update_student (id):
+    query = {"_id":ObjectId(id)}
+    name = request.form['name']
+    country = request.form['country']
+    city = request.form['city']
+    skills = request.form['skills'].split(', ')
+    bio = request.form['bio']
+    birthyear = request.form['birthyear']
+    created_at = datetime.now()
+    student = {
+        'name': name,
+        'country': country,
+        'city': city,
+        'birthyear': birthyear,
+        'skills': skills,
+        'bio': bio,
+        'created_at': created_at
+
+    }
+    db.students.update_one(query, student)
+    # return Response(dumps({"result":"a new student has been created"}), mimetype='application/json')
+    return
+def update_student (id):
+if __name__ == '__main__':
+    # for deployment
+    # to make it work for both production and development
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+
+# Deleting a document using Delete
+from flask import Flask,  Response
+import json
+from bson.objectid import ObjectId
+import json
+from bson.json_util import dumps
+import pymongo
+from datetime import datetime
+
+
+app = Flask(__name__)
+
+#
+MONGODB_URI='mongodb+srv://harshal:your_password@30daysofpython-twxkr.mongodb.net/test?retryWrites=true&w=majority'
+client = pymongo.MongoClient(MONGODB_URI)
+db = client['thirty_days_of_python'] # accessing the database
+
+@app.route('/api/v1.0/students', methods = ['GET'])
+def students ():
+
+    return Response(json.dumps(student), mimetype='application/json')
+@app.route('/api/v1.0/students/<id>', methods = ['GET'])
+def single_student (id):
+    student = db.students.find({'_id':ObjectId(id)})
+    return Response(dumps(student), mimetype='application/json')
+@app.route('/api/v1.0/students', methods = ['POST'])
+def create_student ():
+    name = request.form['name']
+    country = request.form['country']
+    city = request.form['city']
+    skills = request.form['skills'].split(', ')
+    bio = request.form['bio']
+    birthyear = request.form['birthyear']
+    created_at = datetime.now()
+    student = {
+        'name': name,
+        'country': country,
+        'city': city,
+        'birthyear': birthyear,
+        'skills': skills,
+        'bio': bio,
+        'created_at': created_at
+
+    }
+    db.students.insert_one(student)
+    return
+@app.route('/api/v1.0/students/<id>', methods = ['PUT']) # this decorator create the home route
+def update_student (id):
+    query = {"_id":ObjectId(id)}
+    name = request.form['name']
+    country = request.form['country']
+    city = request.form['city']
+    skills = request.form['skills'].split(', ')
+    bio = request.form['bio']
+    birthyear = request.form['birthyear']
+    created_at = datetime.now()
+    student = {
+        'name': name,
+        'country': country,
+        'city': city,
+        'birthyear': birthyear,
+        'skills': skills,
+        'bio': bio,
+        'created_at': created_at
+
+    }
+    db.students.update_one(query, student)
+    # return Response(dumps({"result":"a new student has been created"}), mimetype='application/json')
+    return
+@app.route('/api/v1.0/students/<id>', methods = ['PUT']) # this decorator create the home route
+def update_student (id):
+    query = {"_id":ObjectId(id)}
+    name = request.form['name']
+    country = request.form['country']
+    city = request.form['city']
+    skills = request.form['skills'].split(', ')
+    bio = request.form['bio']
+    birthyear = request.form['birthyear']
+    created_at = datetime.now()
+    student = {
+        'name': name,
+        'country': country,
+        'city': city,
+        'birthyear': birthyear,
+        'skills': skills,
+        'bio': bio,
+        'created_at': created_at
+
+    }
+    db.students.update_one(query, student)
+    # return Response(dumps({"result":"a new student has been created"}), mimetype='application/json')
+    return ;
+@app.route('/api/v1.0/students/<id>', methods = ['DELETE'])
+def delete_student (id):
+    db.students.delete_one({"_id":ObjectId(id)})
+    return
+if __name__ == '__main__':
+    # for deployment
+    # to make it work for both production and development
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
---
+++ 30_Day_Conclusions/30_conclusions.md
@@ -0,0 +1,3 @@
+## Conclusions
+
+During this #30DaysofPython challange I  have learned a lot. It helped me to improve my ability to think logically. 
